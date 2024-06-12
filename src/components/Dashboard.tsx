87cff6b4060ad7a00d9259cc9508103b45cec957
Himanshu Oli <himanshuoliofficial@gmail.com>
2024-06-12 15:52:42 +0530

    Database initailized successfully

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -89,7 +89,7 @@ const Dashboard = () => {
                   href={`/dashboard/${file.id}`}
                   className='flex flex-col gap-2'>
                   <div className='pt-6 px-6 flex w-full items-center justify-between space-x-6'>
-                    <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500' />
+                    <div className='h-10 w-10 flex-shrink-0 rounded-full bg-gradient-to-r from-rose-600 to-violet-600' />
                     <div className='flex-1 truncate'>
                       <div className='flex items-center space-x-3'>
                         <h3 className='truncate text-lg font-medium text-zinc-900'>
